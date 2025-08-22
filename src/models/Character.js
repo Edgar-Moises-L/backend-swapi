d9abc69c2e95b38081d0a52b3ef9d7a627868f55
Edgar-Moises-L <moisesleyva202@gmail.com>
2025-08-21 17:23:26 -0700

    modificación del el modelo Character para permitir relaciones múltiples con Film, Starship y Vehicle, y mantener referencias a Planet y Species

--- src/models/Character.js
+++ src/models/Character.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 const characterSchema = new Schema({
-    name: { type: String, required: true ,unique: true},
+    name: { type: String, required: true, unique: true },
     birth_year: { type: String },
     eye_color: { type: String },
     gender: { type: String },
@@ -10,12 +10,13 @@ const characterSchema = new Schema({
     height: { type: Number },
     mass: { type: Number },
     skin_color: { type: String },
-    films: { type: String }, //falta relacionar
-    homeworld: { type: String }, //falta relacionar
-    species: { type: String }, //falta relacionar
-    starships: { type: String }, //falta relacionar
-    vehicles: { type: String } //falta relacionar
+    films: [{ type: Schema.Types.ObjectId, ref: "Film" }],
+    homeworld: { type: Schema.Types.ObjectId, ref: "Planet" }, 
+    species: { type: Schema.Types.ObjectId, ref: "Species" }, 
+    starships: [{ type: Schema.Types.ObjectId, ref: "Starship" }],
+    vehicles: [{ type: Schema.Types.ObjectId, ref: "Vehicle" }]
 }, { timestamps: true });
+
 
 characterSchema.plugin(mongoosePaginate);
 
--- src/services/characterService.js
+++ src/services/characterService.js
@@ -1,37 +1,68 @@
-import Character from '../models/Character.js'
+import Character from '../models/Character.js';
 
-class characterService {
+const excludeFields = "-createdAt -updatedAt -__v";
+
+class CharacterService {
     async create(character) {
-        return await Character.create(character);
+        const newCharacter = await Character.create(character);
+        return await Character.findById(newCharacter._id)
+            .select(excludeFields)
+            .populate([
+                { path: 'films', select: excludeFields },
+                { path: 'homeworld', select: excludeFields },
+                { path: 'species', select: excludeFields },
+                { path: 'starships', select: excludeFields },
+                { path: 'vehicles', select: excludeFields }
+            ]);
     }
 
     async getAll(page = 1, limit = 10) {
         const options = {
             page,
             limit,
-            select: "-createdAt -updatedAt -__v"
+            select: excludeFields,
+            populate: [
+                { path: 'films', select: excludeFields },
+                { path: 'homeworld', select: excludeFields },
+                { path: 'species', select: excludeFields },
+                { path: 'starships', select: excludeFields },
+                { path: 'vehicles', select: excludeFields }
+            ]
         };
         return await Character.paginate({}, options);
     }
 
-    async getByName(name) {
-        const query = { name: new RegExp(name, 'i') };
-        const options = "-createdAt -updatedAt -__v";
-
-        return await Character.find(query).select(options);
-
+    async getListForCharacter() {
+        return await Character.find({}, "name");
     }
 
     async getById(id) {
-        return await Character.findById(id).select("-createdAt -updatedAt -__v");
+        return await Character.findById(id)
+            .select(excludeFields)
+            .populate([
+                { path: 'films', select: excludeFields },
+                { path: 'homeworld', select: excludeFields },
+                { path: 'species', select: excludeFields },
+                { path: 'starships', select: excludeFields },
+                { path: 'vehicles', select: excludeFields }
+            ]);
     }
 
     async update(id, character) {
-        return await Character.findByIdAndUpdate(id, character, { new: true });
+        return await Character.findByIdAndUpdate(id, character, { new: true })
+            .select(excludeFields)
+            .populate([
+                { path: 'films', select: excludeFields },
+                { path: 'homeworld', select: excludeFields },
+                { path: 'species', select: excludeFields },
+                { path: 'starships', select: excludeFields },
+                { path: 'vehicles', select: excludeFields }
+            ]);
     }
 
     async delete(id) {
-        return await Character.findByIdAndDelete(id);
+        return await Character.findByIdAndDelete(id).lean();
     }
 }
-export default new characterService();
+
+export default new CharacterService();
