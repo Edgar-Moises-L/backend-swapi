68446d9647b9d5c6136444fd6ab5270ec6c5db5e
Edgar-Moises-L <moisesleyva202@gmail.com>
2025-08-28 18:11:43 -0700

    creación de cors para conexión con frontend

--- src/app.js
+++ src/app.js
@@ -6,21 +6,31 @@ import speciesRoute from './routes/speciesRoute.js'
 import starshipRoute from './routes/starshipRoute.js';
 import vehicleRoute from './routes/vehicleRoute.js'
 import express from 'express';
+import cors from 'cors';   // 👈 Importa cors
 import dbClient from './config/dbClient.js'
 import { seedDatabase } from './scripts/dataSeeder.js';
 
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 
 app.use('/api/films', filmRoute);
 app.use('/api/characters', characterRoute);
 app.use('/api/planets', planetRoute);
-app.use('/api/species', speciesRoute)
-app.use('/api/starships', starshipRoute)
-app.use('/api/vehicles', vehicleRoute)
-
+app.use('/api/species', speciesRoute);
+app.use('/api/starships', starshipRoute);
+app.use('/api/vehicles', vehicleRoute);
 
 async function startServer() {
   try {
